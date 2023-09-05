5ad5dd967ebcc42f2b4075c6334517943cb42ba4
Ap01lY0n <itstep22813@gmail.com>
2023-09-05 22:05:29 +0300

    made a new repo because the previous one didn't work

---
+++ src/cat-api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_B4yGLXYDwebECKHyQoHLiNZEIXWUCwSza12oLkJePzdvBGq6juFCKPQFt01Mx563';
+
+function fetchBreeds() {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(res => {
+      return res.data;
+    })
+    .catch(err => {
+      console.log('1', err.response);
+    });
+}
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(res => {
+      return res.data;
+    })
+    .catch(err => console.log('2', err.response));
+}
+export { fetchBreeds, fetchCatByBreed };
